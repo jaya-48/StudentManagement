fd143cce22a95411b6e63d080377d0f79b590c3f
JayaLakshmi <jayalakshmipureti2003@gmail.com>
2026-03-26 21:31:42 +0530

    Basic Crud Operation Project

---
+++ src/main/java/com/example/StudentManagement/Controller/StudentController.java
@@ -0,0 +1,44 @@
+package com.example.StudentManagement.Controller;
+
+import com.example.StudentManagement.Entity.Student;
+import com.example.StudentManagement.Repository.StudentRepository;
+import com.example.StudentManagement.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/StudentManagement")
+public class StudentController {
+
+    //private StudentRepository studentRepository;
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/create")
+    public String createStudent(@RequestBody Student student){
+        return studentService.createStudent(student);
+    }
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+
+    }
+    @GetMapping("/getStudent")
+    public String getStudentById(@PathVariable Long id){
+        return studentService.getStudentById(id);
+    }
+    @PutMapping("/fullUpdation")
+    public String updateFullDetails(@PathVariable Long id,@RequestBody Student student){
+        return studentService.updateFullStudent(id, student);
+    }
+    @PatchMapping("/partialUpdation")
+    public String updatePartialDetails(@PathVariable Long id, @PathVariable int age){
+        return studentService.updatePartialStudent(id, age);
+    }
+    @DeleteMapping("/deleteStudent")
+    public String deleteStudent(@PathVariable Long id){
+        return studentService.deleteStudent(id);
+    }
+
+}
---
+++ src/main/java/com/example/StudentManagement/Service/StudentService.java
@@ -0,0 +1,68 @@
+package com.example.StudentManagement.Service;
+
+import com.example.StudentManagement.Entity.Student;
+import com.example.StudentManagement.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepo;
+
+    public String createStudent(Student student){
+        studentRepo.save(student);
+        return "Student data created successfully";
+    }
+    public List<Student> getAllStudents(){
+        return studentRepo.findAll();
+    }
+    public String getStudentById(Long id){
+        Optional<Student> student=studentRepo.findById(id);
+        if(student.isPresent()){
+            return "Student found";
+        }
+        else{
+            return "Student not found ";
+        }
+    }
+    public String updateFullStudent(Long id,Student student){
+        Optional<Student> student1=studentRepo.findById(id);
+        if(student1.isPresent()){
+            studentRepo.save(student);
+            return "student updated successfully";
+        }
+        else{
+            return "student updation failed";
+        }
+
+    }
+    public String updatePartialStudent(Long id,int age){
+        Optional<Student> student2=studentRepo.findById(id);
+        if(student2.isPresent()){
+            student2.get().setAge(age);
+            studentRepo.save(student2.get());
+            return "Student age updated successfully";
+        }
+        else{
+            return "student updation failed";
+        }
+    }
+    public String deleteStudent(Long id){
+        Optional<Student> student3 = studentRepo.findById(id);
+        if(student3.isPresent()){
+            studentRepo.deleteById(id);
+            return "student deleted successfully";
+        }
+        else{
+            return "student deletion failed";
+        }
+    }
+
+
+
+}
