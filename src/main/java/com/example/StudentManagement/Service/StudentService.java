0a8eb10fe638ceeb30772a4d3d95ef3178e17489
Pureti Jaya Lakshmi <129967163+jaya-48@users.noreply.github.com>
2026-07-28 21:29:04 +0530

    Removed Comments

--- src/main/java/com/example/StudentManagement/Service/StudentService.java
+++ src/main/java/com/example/StudentManagement/Service/StudentService.java
@@ -12,8 +12,6 @@ import java.util.Optional;
 public class StudentService {
 
     @Autowired
-    //Student REpository reference
-    //Student REpository reference2
     private StudentRepository studentRepo;
 
     public Student createStudent(Student student){
