fed09b470c21c1528287bf323f39c7df7d65df64
Pureti Jaya Lakshmi <129967163+jaya-48@users.noreply.github.com>
2026-07-28 21:37:14 +0530

    new 1

--- src/main/java/com/example/StudentManagement/Service/StudentService.java
+++ src/main/java/com/example/StudentManagement/Service/StudentService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 public class StudentService {
 
     @Autowired
+    //new one
     private StudentRepository studentRepo;
 
     public Student createStudent(Student student){
